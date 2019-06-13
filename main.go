14fdc482fcdd4cf7c39700aed3e2027f119048a0
yasrirazak <31167110+yasrirazak@users.noreply.github.com>
2019-06-13 20:36:25 +0700

    Create main.go

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+import (
+ "fmt"
+ "log"
+ "net/http"
+ "os"
+  )
+ 
+func main() {
+http.HandleFunc("/", handler)
+fmt.Println("listening...")
+err := http.ListenAndServe(GetPort(), nil)
+if err != nil {
+log.Fatal("ListenAndServe: ", err)
+}
+}
+  
+func handler(w http.ResponseWriter, r *http.Request) {
+fmt.Fprintf(w, "Hello. This is our first Go web app on Heroku!")
+}
+ 
+
+func GetPort() string {
+var port = os.Getenv("PORT")
+ 
+if port == "" {
+port = "4747"
+fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+}
+return ":" + port
+}
